d3fcfad22061dd905f30ffd08085356ae98b2dd6
sawantdhananjay <dhananjay@pixelbazar.com>
2019-04-10 13:44:01 -0700

    change no to yes child

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,84 +6,131 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
 
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+ <link href="<?php echo base_url('assets/css/bootstrap.min.css');?>" rel="stylesheet">
+ <link href="<?php echo base_url('assets/css/style.css');?>" rel="stylesheet">
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
 </head>
 <body>
+<div class="container-fluid">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 </div>
+<div class="container">
+	<div class="jumbotron ">
+	<h1 class="display-3 text-center">Start Your Will Now</h1>
+	<div class="row text-center">
+		<div class="col-lg-4 ml-5 mt-3">
+			<h2 class="float-right">SEX :</h2>
+		</div>
+		<div class="  col-lg-4 ">
+<div class="row">
 
+	<div class="btn-group btn-group-toggle" data-toggle="buttons">
+
+  <label class="btn btn-primary  btn-lg">
+
+    <input type="radio" name="sex" id="option2" autocomplete="off" value="male"> Male
+  </label>
+  <label class="btn btn-primary btn-lg">
+    <input type="radio" name="sex" id="option3" autocomplete="off" value="female"> Female
+  </label>
+</div>
+</div>
+		</div>
+<div class="col-lg-4">
+
+</div>
+</div>
+<br><br>
+<div class="row ">
+	<div class="col-lg-4 ml-5 mt-3">
+		<h2 class="float-right">Marital Status :</h2>
+	</div>
+	<div class="col-lg-4 ">
+		<div class="row">
+
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+<label class="btn btn-primary btn-lg">
+	<input type="radio" name="married" id="single" autocomplete="off" value="single"> Single
+</label>
+<label class="btn btn-primary btn-lg">
+	<input type="radio" name="married" id="married" autocomplete="off" value="married"> Married
+</label>
+</div>
+</div>
+</div>
+<div class="col-lg-4">
+
+</div>
+</div>
+<br><br>
+<div class="row   " id="child" >
+	<div class="col-lg-4 ml-5 mt-3 ">
+		<h2 class="float-right">Children :</h2>
+	</div>
+	<div class="col-lg-4 " >
+<div class="row">
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+<label class="btn btn-primary btn-lg">
+	<input type="radio" name="child" id="" autocomplete="off" value="1"> Yes
+</label>
+<label class="btn btn-primary btn-lg">
+	<input type="radio" name="child" id="" autocomplete="off" value="0"> No child
+</label>
+</div>
+</div>
+</div>
+<div class="col-lg-4">
+
+</div>
+</div>
+<br><br>
+<div class="row float-right pb-5">
+
+  <p class="lead ">
+    <a class="btn btn-primary btn-lg pl-5 pr-5" href="#" role="button">Next</a>
+  </p>
+	<br><br>
+	</div>
+	<br><br>
+</div>
+</div>
+<script>
+
+$(document).ready(function(){
+		$("#child").hide();
+  $("#married").click(function(){
+    $("#child").toggle();
+  });
+	$("#single").click(function(){
+		$("#child").hide();
+	});
+});
+</script>
 </body>
 </html>
