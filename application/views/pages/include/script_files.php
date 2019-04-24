afd3203e469ef60447c34aa5deba0e0aa8ea0cae
Asif Mulla <asif@pixelbazar.com>
2019-04-24 10:54:14 +0530

    d all page info changes a 24 04 2019

--- application/views/pages/include/script_files.php
+++
@@ -1,5 +0,0 @@
-<script src="<?php echo base_url(); ?>assets/js/jquery-3.3.0.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url(); ?>assets/js/jquery-ui.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url(); ?>assets/js/moment.min.js" type="text/javascript"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" type="text/javascript"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js" type="text/javascript"></script>
